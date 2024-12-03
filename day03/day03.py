c011c8863778ec18f1fbb14bd6b4f722bfbefbff
Pierre Cathe <pierre.cathe@gmail.com>
2024-12-03 14:30:02 +0100

    day 03 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -5,21 +5,46 @@ FILENAME = './input'
 # FILENAME = './example'
 
 MUL_REGEX = "mul\(([0-9]*),([0-9]*)\)"
+DO_REGEX = "do\(\)"
+DONT_REGEX = "don't\(\)"
 
 
 def parse_data(filename):
-    lines = []
-    with open(filename) as data:
-        for raw_line in data:
-            lines.append(raw_line.rstrip())
-    return lines
+    data = ""
+    with open(filename) as lines:
+        for raw_line in lines:
+            data += (raw_line.rstrip())
+    return data
 
 
 def extract_mul_instructions(source_code):
     instructions = []
-    for line in source_code:
-        matches = re.findall(MUL_REGEX, line)
-        instructions.extend([(int(x[0]), int(x[1])) for x in matches])
+    matches = re.findall(MUL_REGEX, source_code)
+    instructions.extend([(int(x[0]), int(x[1])) for x in matches])
+    return instructions
+
+
+def extract_mul_instructions_enabled_only(source_code):
+    instructions = []
+    matches = re.finditer(MUL_REGEX, source_code)
+    dos = sorted(re.finditer(DO_REGEX, source_code), key= lambda match: match.start())
+    donts = sorted(re.finditer(DONT_REGEX, source_code), key= lambda match: match.start())
+    for match in matches:
+        latest_prior_do = 0
+        latest_prior_dont = None
+        for dont in donts:
+            if dont.start() < match.start():
+                latest_prior_dont = dont.start()
+            else:
+                break
+        for do in dos:
+            if do.start() < match.start():
+                latest_prior_do = do.start()
+            else:
+                break
+        if latest_prior_dont is None or latest_prior_do > latest_prior_dont:
+            # either there hasn't been a "don't" yet, or there's been a "do" since then
+            instructions.append((int(match.group(1)), int(match.group(2))))
     return instructions
 
 
@@ -30,6 +55,12 @@ def run():
     for instruction in instructions:
         result_mul += instruction[0] * instruction[1]
     print(f"Result of multiplications : {result_mul}")
+
+    enabled_instructions = extract_mul_instructions_enabled_only(source_code)
+    result_mul = 0
+    for instruction in enabled_instructions:
+        result_mul += instruction[0] * instruction[1]
+    print(f"Result of enabled multiplications : {result_mul}")
 
 
 
